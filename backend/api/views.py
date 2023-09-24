29cdaac0ed54fdca62603d1f51a487c81fe3b434
Евгений Чичин <eugenechichin@yandex.ru>
2023-09-24 04:51:11 +0300

    На проверкy

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,23 +1,24 @@
-from django.db.models import Sum
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
-from django_filters.rest_framework import DjangoFilterBackend
-from djoser.views import UserViewSet
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
-
+from django.db.models import Sum
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
+from djoser.views import UserViewSet
 from .filters import RecipeFilter
 from .permissions import IsAuthorOrReadOnly
 from .serializers import (CustomUserSerializer, FavoriteSerializer,
                           FollowSerializer, IngredientSerializer,
                           RecipeCreateSerializer, RecipeSerializer,
                           TagSerializer)
+
 from recipes.models import (Favorite, Ingredient, Recipe, RecipeIngredient,
                             Shopping_list, Tag)
+
 from users.models import Subscription, User
 
 
